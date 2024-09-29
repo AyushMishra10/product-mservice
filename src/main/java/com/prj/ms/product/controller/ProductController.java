d91452fa6c70a83f2c3375b26224a76d0ee2a0f1
AyushMishra10 <iamayushmishra28@gmail.com>
2024-09-29 17:04:14 +0530

    Modified Endpoint

--- src/main/java/com/prj/ms/product/controller/ProductController.java
+++ src/main/java/com/prj/ms/product/controller/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/product")
 @RequiredArgsConstructor
 public class ProductController {
 	
@@ -21,7 +21,7 @@ public class ProductController {
     @PostMapping("/addProduct")
     @ResponseStatus(HttpStatus.CREATED)
     public ProductResponse createProduct(@RequestBody ProductRequest productRequest){
-        //used dto classes here for decoupling
+        //used DTO classes here for decoupling
     	return productService.createProduct(productRequest);
     }
 
